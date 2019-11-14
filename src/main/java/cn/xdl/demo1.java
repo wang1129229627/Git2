973a7d853c28dbbf8d459d53790a068258ed43a1
wang1129229627 <2648133635@qq.com>
2019-11-14 16:31:14 +0800

    下载Git2到Git3 ssm

--- src/main/java/cn/xdl/demo1.java
+++ src/main/java/cn/xdl/demo1.java
@@ -4,7 +4,7 @@ public class demo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hahahahahahahahhaha");
+		System.out.println("ÏÂÔØGit2µ½Git3");
 	}
 
 }
